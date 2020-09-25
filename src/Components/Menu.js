9d036532b481be6e658291df306a95dfd784e1c0
José Ramón Vergara Ariztía <jrvergara21@gmail.com>
2020-09-25 20:40:24 -0300

    se inica trabajo home y navbar

---
+++ src/Components/Menu.js
@@ -0,0 +1,44 @@
+import React from 'react';
+ 
+class Menu extends React.Component {
+ 
+  render() {
+ 
+   return (
+ 
+   <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+ 
+     <a className="navbar-brand" href="#">Reciclate</a>
+     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+     </button>
+     <div className="collapse navbar-collapse" id="navbarCollapse">
+     <ul className="navbar-nav mr-auto">
+         <li className="nav-item active">
+            <a className="nav-link" href="#">Inicio <span className="sr-only">(current)</span></a>
+         </li>
+         <li className="nav-item">
+            <a className="nav-link" href="#">Nosotros</a>
+         </li>
+         <li className="nav-item">
+            <a className="nav-link" href="#">Quiero Recolectar</a>
+         </li>
+         <li className="nav-item">
+            <a className="nav-link" href="#">Quiero Reciclar</a>
+         </li>
+         <li className="nav-item">
+            <a className="nav-link" href="#">Contacto</a>
+         </li>
+     </ul>
+     
+     </div>
+ 
+ </nav>
+ 
+   )
+    
+  }
+ 
+}
+ 
+export default Menu;
