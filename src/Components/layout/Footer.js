8a5fd092f29faf2788c403d50f8b62b92290a87c
Cristian Parra <cristian.a.parra@gmail.com>
2020-09-26 12:06:24 -0300

    boostrap, react router instalado creando rutas  fotoore y navbar

---
+++ src/Components/layout/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Footer = () => {
+    return (
+
+        <div className="card-footer container">
+            <div className="float-right">
+                <button type="button" className="btn btn-secondary btn-lg">Cancelar</button>
+                <button type="button" className="btn btn-primary btn-lg">Registrarse</button>
+            </div>
+        </div>
+
+    );
+}
+
+export default Footer;
---
+++ src/Components/layout/Navbar.js
@@ -0,0 +1,70 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="card-header container rounded border border-light">
+            <h1 style="">Navbar --------------------------------</h1>
+        </div>
+
+        // <div className="card body container">
+        // 	<div className="alert alert-success" role="alert">
+        //             Registrate como recolector
+        // 	</div>
+        // 	<div className="form-row">
+        // 		<div classNameName="form-group col-md-6">
+        // 			<label for="Card#">Nombres</label>
+        // 			<input type="card" className="form-control" id="card" placeholder=""/>
+        //         </div>
+        // 		<div className="form-group col-md-6">
+        // 			<label for="CVC">Apellidos</label>
+        // 			<input type="card" className="form-control" id="card" placeholder=""/>
+        //         </div>
+        // 	</div>
+        // 	<div className="form-row">
+        // 		<div className="form-group col-md-4">
+        // 			<label for="city">Rut</label>
+        // 			<input type="city" className="form-control" id="city" placeholder=""/>
+        //         </div>
+        // 		<div className="form-group col-md-8 ">
+        // 			<label for="inputState">Dirección</label>
+        // 			<input type="postal-code" className="form-control" id="city" placeholder=""/
+        //         </div>
+        // 	</div>
+        // 	<div className="form-row">
+        // 		<div className="form-group col-md-5">
+        // 			<label for="city">Correo electrónico</label>
+        // 			<input type="city" className="form-control" id="city" placeholder=""/>
+        //         </div>
+        // 		<div className="form-group col-md-7">
+        // 			<label for="last-name">Tipo de actividad</label>
+        // 			<input type="last-name" className="form-control" id="last-name" placeholder=""/>
+        //         </div>
+        // 	</div>
+        // 	<div className="form-row">
+        // 		<div className="form-group col-md-5">
+        // 			<label for="exampleFormControlSelect1">Selecciona el tipo de basura</label>
+        // 			<select className="form-control" id="exampleFormControlSelect1">
+        //                 <option>Plásticos</option>
+        //                 <option>Electrónicos</option>
+        //                 <option>Neumáticos</option>
+        //                 <option>Vidrios</option>
+        //                 <option>Papeles y cartones</option>
+        //                 <option>Desechos orgánicos</option>
+        //                 <option>...</option>
+        //                 <option>....</option>
+        //                 <option>Otros</option>
+        //             </select>
+        // 								</div>
+        // 								<div className="form-group col-md-5">
+        // 									<label for="exampleFormControlInput1">Otros</label>
+        // 									<input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+        //                                     <p>Si seleccionaste otro tipo de basura, especifica.</p>
+
+        //         </div>
+        // 	</div>
+        // </div>
+
+    );
+}
+
+export default Navbar;
